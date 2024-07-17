23e130eee7ccf1bb2090a8814bd3cb176235ca77
Sathya Prakaash <sathyaprakaash112@gmail.com>
2024-07-18 00:27:55 +0530

    Initial Commit

---
+++ src/app/tasks/tasks.component.ts
@@ -0,0 +1,15 @@
+import { Component, EventEmitter, Input, output } from '@angular/core';
+import { TaskComponent } from "./task/task.component";
+
+@Component({
+  selector: 'app-tasks',
+  standalone: true,
+  imports: [TaskComponent],
+  templateUrl: './tasks.component.html',
+  styleUrl: './tasks.component.css'
+})
+export class TasksComponent {
+  @Input({required:true}) name!:string;
+
+
+}
---
+++ src/app/user/user.component.ts
@@ -0,0 +1,27 @@
+import { Component,EventEmitter,Input,Output,computed,signal } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+interface User{
+  id:string,
+  avatar:string
+  name:string
+}
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+  @Input({required:true}) user! : User;
+  @Output() select = new EventEmitter<string>();
+
+  get imagePath(){ 
+    return './assets/users/' + this.user.avatar;
+  }
+  onSelectUser(){
+    this.select.emit(this.user.id); 
+  }
+
+}
