f0d02f8728f4436c3f2af749025637b8717bcd48
Sathya Prakaash <sathyaprakaash112@gmail.com>
2024-07-23 22:57:40 +0530

    Added FormsModule import

--- src/app/tasks/task/newtask/newtask.component.ts
+++ src/app/tasks/task/newtask/newtask.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-newtask',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './newtask.component.html',
   styleUrl: './newtask.component.css'
 })
