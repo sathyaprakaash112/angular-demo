eeeb2f293b274fd992fdd2c6a5a6fa7a77ac5677
Sathya Prakaash <sathyaprakaash112@gmail.com>
2024-08-05 19:30:21 +0530

    Implemented localStorage to store the tasks

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -28,6 +28,13 @@ export class TasksService{
     
     ]
 
+    constructor(){
+      const tasks = localStorage.getItem('tasks');
+      if(tasks){
+        this.tasks = JSON.parse(tasks);
+      }
+    }
+
     getUserTasks(userId:string){
         return this.tasks.filter((task)=> task.userId === userId) 
     }
@@ -40,9 +47,15 @@ export class TasksService{
             title : taskData.title,
             userId: userId
           });
+          this.saveTasks();
     }
 
     removeTask(taskId: string){
         this.tasks = this.tasks.filter((task) => task.id !== taskId);
+        this.saveTasks();
+    }
+
+    private saveTasks(){
+      localStorage.setItem('tasks',JSON.stringify(this.tasks));
     }
 }
